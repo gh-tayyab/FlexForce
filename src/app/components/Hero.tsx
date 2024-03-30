f7eaf7f779c415c5aa2fd2dc97f7d85c3d881bb5
gh-tayyab <muhammadtayyabmt0@gmail.com>
2024-03-30 23:11:16 +0500

    first commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,36 +1,36 @@
 import React from 'react'
 import Image from 'next/image'
-import {FaYoutube} from "react-icons/fa"
+import { FaYoutube } from "react-icons/fa"
 import { trace } from 'next/dist/trace'
 import bg from '../images/bg.jpeg'
 
 function Hero() {
-  return (
-    <div className="min-h-screen flex flex-col md:flex-row justify-center items-center bg-slate-900 md:pt-10 pb-8">
-        <div className='container text-white space-y-6'>
-                <Image
-                data-aos="fade-up"
+    return (
+        <div className="bg min-h-screen flex flex-col md:flex-row justify-center items-center bg-slate-900">
+                <div className='container text-white space-y-6 '>
+            <Image
                 src={bg}
                 alt='bg'
-                className='pt-4'
-                />
-            <h1 data-aos="fade-up" className=' text-4xl sm:text-4xl md:text-6xl w-full md:w-2/4 font-bold leading-tight'>You Only Fail, When You Stop Trying</h1>
-            <p data-aos="fade-up" data-aos-delay="600" className='w-full md:w-2/4 text-sm sm:text-sm md:text-xl'>
-                "You Only Fail, When You Stop Trying" serves as a powerful mantra for gym motivation, reminding us that success is not defined by the number of times we fall, but by our determination to get back up and keep pushing forward. In the world of fitness, setbacks and challenges are inevitable, but it is our resilience and perseverance that ultimately lead to progress. This quote encourages us to embrace failure as a stepping stone to success, recognizing that each attempt, no matter how difficult, brings us closer to our goals. So, the next time you feel like giving up, remember these words and let them fuel your drive to keep going.
-               
-            </p>
-            <div data-aos="fade-up" data-aos-delay="1000" className='flex gap-8'>
-                <button className='uppercase px-4 py-2 bg-orange-500 text-sm hover:scale-105 transition duration-200 ease-in'>Get Started Now</button>
-                <div className='flex gap-2 items-center'>
-                    <FaYoutube size={28} className='text-red-500'/>
-                    <button>Watch Reviews</button>
-                </div>
-               
-            </div>
-        </div>
-    </div>
+                objectFit='cover'
+                objectPosition='fill'
+            />
+                    <h1 data-aos="fade-up" className=' text-4xl sm:text-4xl md:text-6xl w-full md:w-2/4 font-bold leading-tight'>You Only Fail, When You Stop Trying</h1>
+                    <p data-aos="fade-up" data-aos-delay="600" className='w-full md:w-2/4 text-sm sm:text-sm md:text-xl'>
+                        "You Only Fail, When You Stop Trying" serves as a powerful mantra for gym motivation, reminding us that success is not defined by the number of times we fall, but by our determination to get back up and keep pushing forward. In the world of fitness, setbacks and challenges are inevitable, but it is our resilience and perseverance that ultimately lead to progress. This quote encourages us to embrace failure as a stepping stone to success, recognizing that each attempt, no matter how difficult, brings us closer to our goals. So, the next time you feel like giving up, remember these words and let them fuel your drive to keep going.
 
-  )
+                    </p>
+                    <div data-aos="fade-up" data-aos-delay="1000" className='flex gap-8'>
+                        <button className='uppercase px-4 py-2 bg-orange-500 text-sm hover:scale-105 transition duration-200 ease-in'>Get Started Now</button>
+                        <div className='flex gap-2 items-center'>
+                            <FaYoutube size={28} className='text-red-500' />
+                            <button>Watch Reviews</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+        
+
+    )
 }
 
 export default Hero
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,8 @@
 import React from 'react'
 
 import Link from 'next/link'
-import {MdCancel} from "react-icons/md"
-import {CgMenuGridR} from "react-icons/cg"
+import { MdCancel } from "react-icons/md"
+import { CgMenuGridR } from "react-icons/cg"
 import { useState } from 'react'
 import { FaHamburger } from 'react-icons/fa'
 import { IoMdMenu } from 'react-icons/io'
@@ -11,52 +11,53 @@ import { IoMdMenu } from 'react-icons/io'
 
 export const Navlinks = [
   {
-    id:1,
+    id: 1,
     title: "Home",
     link: "/"
   },
   {
-    id:2,
+    id: 2,
     title: "About",
     link: "/About"
   },
   {
-    id:3,
+    id: 3,
     title: "Reviews",
     link: "/Reviews"
   },
   {
-    id:4,
+    id: 4,
     title: "Pricing",
     link: "/Pricing"
   },
   {
-    id:5,
+    id: 5,
     title: "Blogs",
     link: "/Blogs"
   },
 ]
 
 function Navbar() {
-
-
-
+  const [menu, setMenu] = useState(false)
+  const handleChange = () => {
+    setMenu(!menu);
+  }
+  const closeMenu = () => {
+    setMenu(false)
+  }
   return (
     <header className='bg-slate-950 shadow-md text-white backdrop-blur-lg'>
       <nav className='container flex items-center justify-between py-3'>
         <div>
-  
+
           <Link href={"/"}>
             <span className='text-3xl font-bold'>Flex</span>
             <span className='text-3xl font-bold text-orange-500'>Force</span>
           </Link>
         </div>
-        <div className='md:hidden'>
-        <IoMdMenu size={38}/>
-        </div>
         <div className='hidden md:flex'>
           <ul className='flex items-center gap-8'>
-            {Navlinks.map(({id, title, link})=> {
+            {Navlinks.map(({ id, title, link }) => {
               return (
                 <li key={id}>
                   <Link href={link} className='text-lg font-medium hover:text-orange-400 transition-all'>{title}</Link>
@@ -65,8 +66,23 @@ function Navbar() {
             })}
           </ul>
         </div>
+        <div className='md:hidden flex items-center'>
+          {menu ? (
+            <MdCancel size={25} onClick={handleChange} />
+          ) : <CgMenuGridR size={25} onClick={handleChange} />
+          }
+        </div>
       </nav>
-     
+      <div className={`${menu ? "translate-x-0 " : "-translate-x-full"} md:hidden flex flex-col absolute bg-black z-10 left-0 top-14 list-none font-medium text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+            {Navlinks.map(({id, title, link})=>{
+                  return (
+                    <li key={id}>
+                      <Link href={link} onClick={closeMenu} className='text-lg font-medium hover:text-orange-400 transition-all'>{title}
+                      </Link>
+                    </li>
+                  )
+            })}
+      </div>
     </header>
   )
 }
